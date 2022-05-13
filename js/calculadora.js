7a8e73011c19944756585a26df0407c6245cd2ab
nikolostt <niko.lost@gmail.com>
2022-05-13 11:03:31 -0400

    actualización index y js

--- js/calculadora.js
+++ js/calculadora.js
@@ -3,7 +3,7 @@ function accion(valor) {
     
     if (valor == "Limpiar"){
         document.getElementById("tResultado").value = "";
-    } else if (valor == "Resultado") {
+    } else if (valor == "=") {
         document.getElementById("tResultado").value = eval(document.getElementById("tResultado").value);
     } else if (!isNaN(valor) || valor == "+" || valor == '-' || valor == '*' || valor == '/'){
         document.getElementById("tResultado").value += valor;
